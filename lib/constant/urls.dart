79461b873d5c405cd25e33f6fcf3245d52cf5dd0
Sanni Shafeeq <62085280+sashad97@users.noreply.github.com>
2021-11-22 11:26:05 +0100

    Update urls.dart

--- lib/constant/urls.dart
+++ lib/constant/urls.dart
@@ -12,5 +12,5 @@ class Paths {
   static const String ipAddressUrl = 'http://ip-api.com/json';
   static const String bytimeUrl = '/data/2.5/onecall/timemachine?';
   static const String timeMach = '&dt=';
-  static const String place_apikey = 'Provided on request';
+  static const String place_apikey = 'AIzaSyBVDPUIilR05t1f0JaWf6mR0rvDvW6Vy1Y';
 }
